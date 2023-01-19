32640672711d930b8f452f3b68cfa4a99fa66b64
Manikandan Palanisamy <manikandanp@cdata.com>
2023-01-19 16:42:04 +0530

    Add data source

--- src/Salesforce.java
+++ src/Salesforce.java
@@ -1,8 +1,8 @@
 import java.sql.*;
 public class Salesforce {
-    public static void main(String args[]) throws SQLException {
+    public static void main(String[] args) throws SQLException {
         //Registering the Driver
-        Connection connection = null;
+        Connection connection;
         connection = DriverManager.getConnection("jdbc:salesforce:User=support@nsoftware.com;Password=!nBanana1111;Security Token=DCvIH8H8P0yCQE41X6tA7SS48;");
         Statement stat = connection.createStatement();
         boolean ret = stat.execute("SELECT BillingState, Name FROM Account");
